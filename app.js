0561a58e5dabbbdd10fb148e658589590b1913f1
Padol Nitin <109783378+PadolNitin0509@users.noreply.github.com>
2022-07-27 17:11:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World! This node js file on git');
-   res.end('Hi this is new repository');
+  res.end('Hello World! This is nitin padol');
+  res.end('Hi this is new repository');
 }).listen(8080);
